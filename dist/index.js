0adaf04fae26fcde905b240f9798dc97e9a5b0b1
Philipp Burckhardt <pburckhardt@outlook.com>
2022-06-27 21:43:39 -0400

    Add copyright notice

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,26 @@
 "use strict";
-// MODULES //
+/**
+* @license Apache-2.0
+*
+* Copyright (c) 2022 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+// MODULES //
 const github_1 = require("@actions/github");
 const error_tools_pkg2id_1 = __importDefault(require("@stdlib/error-tools-pkg2id"));
 const error_tools_msg2id_1 = __importDefault(require("@stdlib/error-tools-msg2id"));
